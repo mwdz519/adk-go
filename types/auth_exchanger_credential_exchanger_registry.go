aec5f3123a2f91d0df33afd98b2d114af454b216
Koichi Shiraishi <zchee.io@gmail.com>
2025-07-08 15:26:40 +0900

    types: move CredentialExchangerRegistry and CredentialRefresherRegistry
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- types/auth_exchanger_credential_exchanger_registry.go
+++ types/auth_exchanger_credential_exchanger_registry.go
@@ -1,11 +1,7 @@
 // Copyright 2025 The Go A2A Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package exchanger
-
-import (
-	"github.com/go-a2a/adk-go/types"
-)
+package types
 
 // CredentialExchangerRegistry registry for credential exchanger instances.
 //
@@ -14,23 +10,23 @@ import (
 // This feature is experimental and may change or be removed in future versions without notice. It may
 // introduce breaking changes at any time.
 type CredentialExchangerRegistry struct {
-	exchangers map[types.AuthCredentialTypes]types.CredentialExchanger
+	exchangers map[AuthCredentialTypes]CredentialExchanger
 }
 
 // NewCredentialExchangerRegistry returns the new [CredentialExchangerRegistry].
 func NewCredentialExchangerRegistry() *CredentialExchangerRegistry {
 	return &CredentialExchangerRegistry{
-		exchangers: make(map[types.AuthCredentialTypes]types.CredentialExchanger),
+		exchangers: make(map[AuthCredentialTypes]CredentialExchanger),
 	}
 }
 
 // Register registry for credential exchanger instances.
-func (e *CredentialExchangerRegistry) Register(credentialType types.AuthCredentialTypes, exchanger types.CredentialExchanger) {
+func (e *CredentialExchangerRegistry) Register(credentialType AuthCredentialTypes, exchanger CredentialExchanger) {
 	e.exchangers[credentialType] = exchanger
 }
 
 // GetExchanger get the exchanger for a credential type.
-func (e *CredentialExchangerRegistry) GetExchanger(credentialType types.AuthCredentialTypes) (types.CredentialExchanger, bool) {
+func (e *CredentialExchangerRegistry) GetExchanger(credentialType AuthCredentialTypes) (CredentialExchanger, bool) {
 	exchanger, ok := e.exchangers[credentialType]
 	return exchanger, ok
 }
--- types/auth_refresher_credential_refresher_registry.go
+++ types/auth_refresher_credential_refresher_registry.go
@@ -1,11 +1,7 @@
 // Copyright 2025 The Go A2A Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package refresher
-
-import (
-	"github.com/go-a2a/adk-go/types"
-)
+package types
 
 // CredentialRefresherRegistry registry for credential refresher instances.
 //
@@ -14,23 +10,23 @@ import (
 // This feature is experimental and may change or be removed in future versions without notice. It may
 // introduce breaking changes at any time.
 type CredentialRefresherRegistry struct {
-	refreshers map[types.AuthCredentialTypes]types.CredentialRefresher
+	refreshers map[AuthCredentialTypes]CredentialRefresher
 }
 
 // NewCredentialRefresherRegistry returns the new [CredentialRefresherRegistry].
 func NewCredentialRefresherRegistry() *CredentialRefresherRegistry {
 	return &CredentialRefresherRegistry{
-		refreshers: make(map[types.AuthCredentialTypes]types.CredentialRefresher),
+		refreshers: make(map[AuthCredentialTypes]CredentialRefresher),
 	}
 }
 
 // Register register a refresher instance for a credential type.
-func (r *CredentialRefresherRegistry) Register(credentialType types.AuthCredentialTypes, refresher types.CredentialRefresher) {
+func (r *CredentialRefresherRegistry) Register(credentialType AuthCredentialTypes, refresher CredentialRefresher) {
 	r.refreshers[credentialType] = refresher
 }
 
 // GetRefresher get the refresher instance for a credential type.
-func (r *CredentialRefresherRegistry) GetRefresher(credentialType types.AuthCredentialTypes) (types.CredentialRefresher, bool) {
+func (r *CredentialRefresherRegistry) GetRefresher(credentialType AuthCredentialTypes) (CredentialRefresher, bool) {
 	refresher, ok := r.refreshers[credentialType]
 	return refresher, ok
 }
