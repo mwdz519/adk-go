d13c2b3a00b5997c9656882225755a3c11e51ab5
Koichi Shiraishi <zchee.io@gmail.com>
2025-07-11 07:43:55 +0900

    types: reorder type and unify SecurityScheme interface
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- types/auth_schemas.go
+++ types/auth_schemas.go
@@ -3,22 +3,19 @@
 
 package types
 
-// OAuthGrantType represents the OAuth2 flow (or grant type).
-type OAuthGrantType string
-
-const (
-	ClientCredentialsGrant OAuthGrantType = "client_credentials"
-	AuthorizationCodeGrant OAuthGrantType = "authorization_code"
-	ImplicitGrant          OAuthGrantType = "implicit"
-	PasswordGrant          OAuthGrantType = "password"
-)
-
 // AuthScheme represents either a [SecurityScheme] or [OpenIDConnectWithConfig].
 type AuthScheme interface {
-	isAuthScheme()
 	AuthType() AuthCredentialTypes
 }
 
+// SecurityScheme represents a security scheme that implements the [AuthScheme] interface.
+type SecurityScheme interface {
+	AuthScheme
+
+	isSecurityScheme()
+}
+
+// OpenIDConnectWithConfig represents an OpenID Connect configuration with additional endpoints and methods.
 type OpenIDConnectWithConfig struct {
 	Type                              AuthCredentialTypes `json:"type"`
 	AuthorizationEndpoint             string              `json:"authorization_endpoint"`
@@ -32,67 +29,63 @@ type OpenIDConnectWithConfig struct {
 
 var _ AuthScheme = (*OpenIDConnectWithConfig)(nil)
 
-func (*OpenIDConnectWithConfig) isAuthScheme() {}
-
 // AuthType returns a string representation of the [OpenIDConnectWithConfig] type.
 func (a *OpenIDConnectWithConfig) AuthType() AuthCredentialTypes {
 	return a.Type
 }
 
-type SecurityScheme interface {
-	isAuthScheme()
-	isSecurityScheme()
-}
+// APIKeyIn represents the location of an API key in a request.
+type APIKeyIn string
 
+const (
+	// InQuery indicates the API key is passed in the url query parameters.
+	InQuery APIKeyIn = "query"
+	// InHeader indicates the API key is passed in the request header.
+	InHeader APIKeyIn = "header"
+	// InCookie indicates the API key is passed in a cookie.
+	InCookie APIKeyIn = "cookie"
+)
+
+// APIKeySecurityScheme represents an API key security scheme.
 type APIKeySecurityScheme struct {
 	Type AuthCredentialTypes `json:"type"`
-	In   string              `json:"in,omitzero"`
+	In   APIKeyIn            `json:"in,omitzero"`
 	Name string              `json:"name,omitzero"`
 }
 
-var (
-	_ SecurityScheme = (*APIKeySecurityScheme)(nil)
-	_ AuthScheme     = (*APIKeySecurityScheme)(nil)
-)
+var _ SecurityScheme = (*APIKeySecurityScheme)(nil)
 
 func (*APIKeySecurityScheme) isSecurityScheme() {}
-func (*APIKeySecurityScheme) isAuthScheme()     {}
 
 // AuthType returns a string representation of the [APIKeySecurityScheme] type.
 func (a *APIKeySecurityScheme) AuthType() AuthCredentialTypes {
 	return a.Type
 }
 
+// HTTPBaseSecurityScheme represents a base HTTP security scheme.
 type HTTPBaseSecurityScheme struct {
 	Type   AuthCredentialTypes `json:"type"`
 	Scheme string              `json:"scheme,omitzero"`
 }
 
-var (
-	_ SecurityScheme = (*HTTPBaseSecurityScheme)(nil)
-	_ AuthScheme     = (*HTTPBaseSecurityScheme)(nil)
-)
+var _ SecurityScheme = (*HTTPBaseSecurityScheme)(nil)
 
 func (*HTTPBaseSecurityScheme) isSecurityScheme() {}
-func (*HTTPBaseSecurityScheme) isAuthScheme()     {}
 
 // AuthType returns a string representation of the [HTTPBaseSecurityScheme] type.
 func (a *HTTPBaseSecurityScheme) AuthType() AuthCredentialTypes {
 	return a.Type
 }
 
+// OAuth2SecurityScheme represents an OAuth2 security scheme with various flows.
 type OAuth2SecurityScheme struct {
 	Type  AuthCredentialTypes `json:"type"`
 	Flows *OAuthFlows         `json:"flows"`
 }
 
-var (
-	_ SecurityScheme = (*OAuth2SecurityScheme)(nil)
-	_ AuthScheme     = (*OAuth2SecurityScheme)(nil)
-)
+var _ SecurityScheme = (*OAuth2SecurityScheme)(nil)
 
 func (*OAuth2SecurityScheme) isSecurityScheme() {}
-func (*OAuth2SecurityScheme) isAuthScheme()     {}
 
 // AuthType returns a string representation of the [OAuth2SecurityScheme] type.
 func (a *OAuth2SecurityScheme) AuthType() AuthCredentialTypes {
@@ -115,18 +108,15 @@ type OAuthFlows struct {
 	AuthorizationCode *OAuthFlow `json:"authorizationCode,omitzero"`
 }
 
+// OpenIdConnectSecurityScheme represents an OpenID Connect security scheme.
 type OpenIdConnectSecurityScheme struct {
 	Type             AuthCredentialTypes `json:"type"`
 	OpenIDConnectURL string              `json:"openIdConnectUrl"`
 }
 
-var (
-	_ SecurityScheme = (*OpenIdConnectSecurityScheme)(nil)
-	_ AuthScheme     = (*OpenIdConnectSecurityScheme)(nil)
-)
+var _ SecurityScheme = (*OpenIdConnectSecurityScheme)(nil)
 
 func (*OpenIdConnectSecurityScheme) isSecurityScheme() {}
-func (*OpenIdConnectSecurityScheme) isAuthScheme()     {}
 
 // AuthType returns a string representation of the [OpenIdConnectSecurityScheme] type.
 func (a *OpenIdConnectSecurityScheme) AuthType() AuthCredentialTypes {
@@ -150,6 +140,27 @@ func GetAuthSchemeType(scheme AuthScheme) AuthCredentialTypes {
 		return ""
 	}
 }
+
+// OAuthGrantType represents the OAuth2 flow or grant type.
+type OAuthGrantType string
+
+const (
+	// ClientCredentialsGrant represents the client credentials grant type.
+	// See RFC 6749 Section 4.4.
+	ClientCredentialsGrant OAuthGrantType = "client_credentials"
+
+	// AuthorizationCodeGrant represents the authorization code grant type.
+	// See RFC 6749 Section 4.1.
+	AuthorizationCodeGrant OAuthGrantType = "authorization_code"
+
+	// ImplicitGrant represents the implicit grant type.
+	// See RFC 6749 Section 4.2.
+	ImplicitGrant OAuthGrantType = "implicit"
+
+	// PasswordGrant represents the password grant type.
+	// See RFC 6749 Section 4.3.
+	PasswordGrant OAuthGrantType = "password"
+)
 
 // FromOAuthFlows determines the grant type from OAuthFlows.
 func FromOAuthFlows(flows *OAuthFlows) OAuthGrantType {
